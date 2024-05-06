c09aea9c475dcd49c3e32270cc15f1e04e53d984
Mariia <mariia.bieskova@gmail.com>
2024-05-06 13:28:18 +0300

    create rows and cells and style them to fill in the whole size of the container irrelevant of the number of the cells

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 function createGrid(num) {
     const grid = document.querySelector("div#container");
 
-    for (i = 1; i <= num*num; i++){
-        const cell = document.createElement("div");
-        cell.setAttribute("class", "cell");
-        cell.id = i;
-        grid.appendChild(cell);
+    for (i = 1; i <= num; i++){
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        row.id = i;
+        grid.appendChild(row); 
+
+        for (j = 1; j<= num; j++){
+            const cell = document.createElement("div");
+            cell.setAttribute("class", "cell");
+            cell.id = j;
+            row.appendChild(cell);
+    
+        }
        
     }
 
+
 }
 
-createGrid(16);
+createGrid(39);
 
 
 
